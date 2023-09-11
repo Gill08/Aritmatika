99414e26c3387e8ee574c6fa414a7a7dda1dbf2d
Gill08 <144661722+Gill08@users.noreply.github.com>
2023-09-11 14:40:51 +0700

    Create .cpp

---
+++ .cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+int a, b, c; 
+cout << " program operator Aritmatika \n";
+cout <<" masukan angka pertama:";
+cin >> a; 
+cout << " masukan angka kedua:";
+cin >> b;
+cout << "Hasil penjumlahan adalah: "<< a + b << endl;
+cout << "Hasil pengurangan adalah: "<< a - b << endl;
+cout << "Hasil perkalian adalah: "<< a * b << endl;
+cout << "Hasil pembagian adalah: "<< a / b << endl;
+return 0; 
+}
